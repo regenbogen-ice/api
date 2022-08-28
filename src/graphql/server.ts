a2cba5463ca9141b74f160a274c8bd84a418bc39
AdriDevelopsThings <adrianbloober@gmail.com>
2022-08-28 20:47:23 +0200

    add response cache plugin

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -2,10 +2,14 @@ import { ApolloServer } from 'apollo-server-express'
 import { randomBytes } from 'crypto'
 import { readFileSync } from 'fs'
 import resolvers from './resolvers/resolvers.js'
+import { default as origResponseCachePlugin } from 'apollo-server-plugin-response-cache';
+
+type PluginType = typeof origResponseCachePlugin
+const responseCachePlugin = (origResponseCachePlugin as any).default as PluginType
 
 export const apolloServer = new ApolloServer({
     typeDefs: readFileSync(process.env.GRAPHQL_SCHEMA_PATH || 'schema.graphql', { encoding: 'utf-8' }),
-    resolvers: resolvers,
+    resolvers,
     cache: 'bounded',
     introspection: true,
     formatError: (error) => {
@@ -16,7 +20,10 @@ export const apolloServer = new ApolloServer({
             message: 'Internal server error.',
             error_code: errorCode
         }
-    }
+    },
+    plugins: [
+        responseCachePlugin(),
+    ]
 })
 
 await apolloServer.start()
