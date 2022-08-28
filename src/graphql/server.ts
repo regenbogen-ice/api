533399f18c46804e78f83f9a2d91d002d644eec2
AdriDevelopsThings <adrianbloober@gmail.com>
2022-08-28 16:03:29 +0200

    add error handling

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from 'apollo-server-express'
+import { randomBytes } from 'crypto'
 import { readFileSync } from 'fs'
 import resolvers from './resolvers/resolvers.js'
 
@@ -8,7 +9,13 @@ export const apolloServer = new ApolloServer({
     cache: 'bounded',
     introspection: true,
     formatError: (error) => {
-        return new Error(`Internal server error.`)
+        const errorCode = randomBytes(16).toString('hex')
+        console.error(`Error ${errorCode} while request: ${error}`)
+        console.error(error.stack)
+        return {
+            message: 'Internal server error.',
+            error_code: errorCode
+        }
     }
 })
 
