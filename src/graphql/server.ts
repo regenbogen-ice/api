d99346a4a5188a9c5ae85f1a31c3f92fa783c9ae
AdriDevelopsThings <adrianbloober@gmail.com>
2022-08-28 15:48:21 +0200

    enable introspection graphql

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -6,6 +6,7 @@ export const apolloServer = new ApolloServer({
     typeDefs: readFileSync(process.env.GRAPHQL_SCHEMA_PATH || 'schema.graphql', { encoding: 'utf-8' }),
     resolvers: resolvers,
     cache: 'bounded',
+    introspection: true,
     formatError: (error) => {
         return new Error(`Internal server error.`)
     }
